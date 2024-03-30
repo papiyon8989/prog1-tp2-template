02dc2ecf16fc926650b00428b71d990f5ec4fe56
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-03-30 22:31:21 +0100

    Entrega chage.py

--- change.py
+++ change.py
@@ -1,3 +1,12 @@
 def change():
     expense = 23.75
     money = 100
+    print(f"Ingresar gasto:")
+    print(f"{expense}")
+    print("Dinero recibido")
+    print(f"{money}\n")
+    print(f"{"Vuelto"}\n")
+    print(f"Pesos:")
+    print(f"{int(money - expense)}")
+    print(f"Centavos:")
+    print(f"{int(money % expense)**2}")
